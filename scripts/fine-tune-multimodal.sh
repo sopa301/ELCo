8358e9dd09b99a5cbe14990a623cf0d62dac9036
sopa301 <96387349+sopa301@users.noreply.github.com>
2025-04-08 14:24:50 +0800

    run bart large

--- scripts/fine-tune-multimodal.sh
+++ scripts/fine-tune-multimodal.sh
@@ -4,10 +4,10 @@
 huggingface_path=./huggingface-emoji/
 
 # Multimodal models
-for model_name in roberta-base
+for model_name in bart-large
 
 do
-    for seed in 47
+    for seed in 43 44 45 46 47
     do
         CUDA_VISIBLE_DEVICES=0 python scripts/emote_multimodal.py --finetune 1 --model_name $model_name --portion 1 --seed $seed --hfpath $huggingface_path --use_images
     done
